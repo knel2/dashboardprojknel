4153c3db8fe709bb7214c96f907321ac7b59b1e9
knel2 <knel2@uw.edu>
2026-02-19 10:49:30 -0800

    added everything from my other repository that didn't work

---
+++ js/main.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken ='pk.eyJ1Ijoia25lbDIiLCJhIjoiY21rdTlkYmx5MThyZjNmcHVrMDYzdXJ4dyJ9.HrXpvWOlXxo7fGCGnGQG6A';
+
+// declare the map object
+let map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/dark-v10',
+    zoom: 5, // starting zoom
+    minZoom: 5,
+    center: [138, 38] // starting center
+});
